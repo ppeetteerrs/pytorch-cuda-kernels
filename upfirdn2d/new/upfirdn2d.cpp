0c173542f75593c64e81d783d34ac8f9e3ec46be
Peter Yuen <ppeetteerrs@Peter.localdomain>
2022-01-03 17:44:30 +0800

    Changed to spaces

--- upfirdn2d/new/upfirdn2d.cpp
+++ upfirdn2d/new/upfirdn2d.cpp
@@ -3,34 +3,34 @@
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) \
-	TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+    TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) \
-	CHECK_CUDA(x);     \
-	CHECK_CONTIGUOUS(x)
+    CHECK_CUDA(x);     \
+    CHECK_CONTIGUOUS(x)
 
 // Forward declaration
 torch::Tensor upfirdn2d_op(const torch::Tensor& input,
-						   const torch::Tensor& kernel, int up_x, int up_y,
-						   int down_x, int down_y, int pad_x0, int pad_x1,
-						   int pad_y0, int pad_y1, bool force_generic);
+                           const torch::Tensor& kernel, int up_x, int up_y,
+                           int down_x, int down_y, int pad_x0, int pad_x1,
+                           int pad_y0, int pad_y1, bool force_generic);
 
 // Python function definition
 torch::Tensor upfirdn2d(const torch::Tensor& input, const torch::Tensor& kernel,
-						int up_x, int up_y, int down_x, int down_y, int pad_x0,
-						int pad_x1, int pad_y0, int pad_y1,
-						bool force_generic = false) {
-	// Check input validity
-	CHECK_INPUT(input);
-	CHECK_INPUT(kernel);
+                        int up_x, int up_y, int down_x, int down_y, int pad_x0,
+                        int pad_x1, int pad_y0, int pad_y1,
+                        bool force_generic = false) {
+    // Check input validity
+    CHECK_INPUT(input);
+    CHECK_INPUT(kernel);
 
-	// Sets default CUDA device, and reset it when destructed.
-	at::DeviceGuard guard(input.device());
+    // Sets default CUDA device, and reset it when destructed.
+    at::DeviceGuard guard(input.device());
 
-	return upfirdn2d_op(input, kernel, up_x, up_y, down_x, down_y, pad_x0,
-						pad_x1, pad_y0, pad_y1, force_generic);
+    return upfirdn2d_op(input, kernel, up_x, up_y, down_x, down_y, pad_x0,
+                        pad_x1, pad_y0, pad_y1, force_generic);
 }
 
 // Defines function upfirdn2d inside Pytorch extension
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-	m.def("upfirdn2d", &upfirdn2d, "upfirdn2d (CUDA)");
+    m.def("upfirdn2d", &upfirdn2d, "upfirdn2d (CUDA)");
 }
